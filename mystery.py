f945d4512b3d4e982dd506d3ddc0bab121e556dc
mack66hm <mack66hm@gmail.com>
2020-02-20 12:44:05 -0500

    created start of game board

---
+++ mystery.py
@@ -0,0 +1,17 @@
+
+
+class Game:
+    def __init__(self):
+        pass
+
+
+class Player:
+    def __init__(self):
+        pass
+
+
+class Computer:
+    def __init__(self):
+        pass 
+
+Game()
