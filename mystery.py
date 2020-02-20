12c12d1b93c374e005902c13367bb5a3339c9513
mack66hm <mack66hm@gmail.com>
2020-02-20 15:15:47 -0500

    fixed classes

--- mystery.py
+++ mystery.py
@@ -1,17 +1,23 @@
-
+import random
+# WORDLIST = 'words.txt'
 
 class Game:
     def __init__(self):
+        self.player = Player("Player")
+
+
+    def get_random_word(min_word_length):
+        """ Getting random word from list."""
         pass
+       
 
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        
+    def __str__(self):
+        return f'{self.name}'
 
-
-class Computer:
-    def __init__(self):
-        pass 
 
 Game()
